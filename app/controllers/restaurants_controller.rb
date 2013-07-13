318b93994db2d6549a121a9fa0c6f0cb4679691a
James Cook <jamesmarkcook@gmail.com>
2013-07-13 17:22:05 -0400

    Converted restaurant opening time into string to store in DB. Added page titles.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
 
   def index
-
+    @title = "Restaurants"
     @restaurants = Restaurant.all
     
   end
@@ -9,11 +9,11 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
 
-    params[:restaurant][:opening_time] = parsed_time(params[:restaurant]["opening_time(4i)"], params[:restaurant]["opening_time(5i)"])
-    params[:restaurant][:closing_time] = parsed_time(params[:restaurant]["closing_time(4i)"], params[:restaurant]["closing_time(5i)"])
+    @restaurant.opening_time = parsed_time(params[:restaurant]["opening_time(4i)"], params[:restaurant]["opening_time(5i)"])
+    @restaurant.closing_time = parsed_time(params[:restaurant]["closing_time(4i)"], params[:restaurant]["closing_time(5i)"])
       
       
-
+    
       
     @restaurant.save
     redirect_to restaurants_path
@@ -21,6 +21,7 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @title = "Add a Restaurant"
     @restaurant = Restaurant.new
     @cuisine = Cuisine.all
   end
